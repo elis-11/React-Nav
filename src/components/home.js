d337b320bcbcf2506e7e3a98f36d9913e8ae7fbb
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-05 16:33:18 +0200

    TO DO : UPDATE MONTAIN.BG

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,16 @@
-import mountainBG from "./images/mountainBG.jpg";
+import { Link } from "react-router-dom";
+import "../css/nav.css";
 
-
-function Home() {
+function Home(props) {
   return (
-    <div className="page-component home-component" style={{backgroundImage: `url(${mountainBG})`}}>
-      <h1>React Router</h1>
+    <div
+      className="page-component home-component"
+      style={{ backgroundImage: `url(${props.mountainBG})` }}
+    >
+      <h2>React Router</h2>
+      <Link className="call-to-action-contact-link" to="/contact">
+        Here me
+      </Link>
     </div>
   );
 }
---
+++ src/components/loader.js
@@ -0,0 +1,11 @@
+import "../css/loader.css"
+
+function Loader() {
+
+}
+
+<div class="main">
+  <div class="one"></div>
+  <div class="two"></div>
+  <div class="three"></div>
+</div>
