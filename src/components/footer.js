82633cc3ebf18a2b7013654d906f121854e5cd14
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-06 13:35:58 +0200

    npm install sass

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,54 +3,84 @@ import "../css/footer.css";
 function Footer() {
   return (
     <footer>
-      <ul className="footer-ul">
+      <ul className="footer">
         <li>
+        <a
+            href="https://github.com/elis-11"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
           <img
-            alt="Facebook icon"
-            src="https://img.icons8.com/office/40/000000/facebook.png"
-          />
-        </li>
-        <li>
-          <img
-            alt="Instagram icon"
-            src="https://img.icons8.com/offices/40/000000/instagram-new.png"
-          />
-        </li>
-        <li>
-          <img
-            alt="LinkedIn icon"
-            src="https://img.icons8.com/offices/40/000000/linkedin.png"
-          />
-        </li>
-        <li>
-          <img
-            alt="Stack Overflow icon"
-            src="https://img.icons8.com/officexs/40/000000/whatsapp.png"
-          />
-        </li>
-        <li>
-          <img
+            className="i"
             alt="Stack Overflow icon"
             src="https://img.icons8.com/color/45/000000/stackoverflow.png"
           />
+          </a>
         </li>
         <li>
+          <a
+            href="https://github.com/elis-11"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
           <img
-            alt="Stack Overflow icon"
-            src="https://img.icons8.com/fluency/48/000000/mail.png"
-          />
-        </li>
-        <li>
-          <img
+            className="i"
             alt="Github icon"
             src="https://img.icons8.com/nolan/46/github.png"
-          />
+            />
+          </a>
         </li>
         <li>
+        <a
+          href="www.linkedin.com/in/eliza.arzanukaeva"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
-            alt="Gmail  icon"
-            src="https://img.icons8.com/dotty/40/000000/angry-face-meme.png"
+            className="i"
+            alt="LinkedIn icon"
+            src="https://img.icons8.com/offices/40/000000/linkedin.png"
+            />
+            </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/elis-11"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <img
+            className="i"
+            alt="Stack Overflow icon"
+            src="https://img.icons8.com/officexs/40/000000/whatsapp.png"
           />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://mail.com/eliza.arzanukaeva"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <img
+            className="i"
+            alt="Gmail icon"
+            src="https://img.icons8.com/fluency/48/000000/important-mail.png"
+            />
+            </a>
+        </li>
+        <li>
+          <a
+            href="https://facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <img
+            className="i"
+            alt="Facebook icon"
+            src="https://img.icons8.com/office/40/000000/facebook.png"
+            />
+            </a>
         </li>
       </ul>
     </footer>
